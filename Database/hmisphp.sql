ed3fe99e2171a6b2876303c0cb95eb1fe918c6db
3phy <barangan.jb.bscs@gmail.com>
2025-06-19 22:37:23 +0800

    updated db

--- Database/hmisphp.sql
+++ Database/hmisphp.sql
@@ -2,8 +2,8 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: May 27, 2025 at 09:22 AM
+-- Host: localhost:3306
+-- Generation Time: Jun 19, 2025 at 04:34 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -41,7 +41,7 @@ CREATE TABLE `his_admin` (
 --
 
 INSERT INTO `his_admin` (`ad_id`, `ad_fname`, `ad_lname`, `ad_email`, `ad_pwd`, `ad_dpic`) VALUES
-(1, 'System', 'Administrator', 'barangan.jb.bscs@gmail.com', 'admin123', 'doc-icon.png');
+(1, 'System', 'Administrator', 'barangan.jb.bscs@gmail.com', 'admin', 'doc-icon.png');
 
 -- --------------------------------------------------------
 
@@ -57,6 +57,13 @@ CREATE TABLE `his_consultations` (
   `consult_checklist` text DEFAULT NULL,
   `consult_image` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `his_consultations`
+--
+
+INSERT INTO `his_consultations` (`consult_id`, `pat_id`, `consult_date`, `consult_notes`, `consult_checklist`, `consult_image`) VALUES
+(18, 31, '2025-06-19 00:53:35', 'ds', ', asd, asd, as', '');
 
 -- --------------------------------------------------------
 
@@ -80,7 +87,8 @@ CREATE TABLE `his_docs` (
 --
 
 INSERT INTO `his_docs` (`doc_id`, `doc_fname`, `doc_lname`, `doc_email`, `doc_pwd`, `doc_dept`, `doc_number`, `doc_dpic`) VALUES
-(14, 'Doc', 'McStuffin', 'docsample@mail.com', 'fe703d258c7ef5f50b71e06565a65aa07194907f', NULL, 'UNVK9', 'menu_icon_150667.png');
+(20, 'John Bill', 'Barangan', 'patientsussystem@gmail.com', 'b6e021fc48fe90673b5e09ccbe978946575bd268', NULL, '256-05760M', 'RobloxScreenShot20250530_230457091.png'),
+(21, 'John Bill', 'Barangan', '1@gmail.com', '618e02fc80fa3a0bd41d65f5b54a11fc50426d12', 'Physical Department', '1', NULL);
 
 -- --------------------------------------------------------
 
@@ -94,10 +102,12 @@ CREATE TABLE `his_patients` (
   `pat_lname` varchar(200) DEFAULT NULL,
   `pat_dob` varchar(200) DEFAULT NULL,
   `pat_age` varchar(200) DEFAULT NULL,
+  `pat_gender` varchar(20) DEFAULT NULL,
   `pat_number` varchar(200) DEFAULT NULL,
   `pat_addr` varchar(200) DEFAULT NULL,
   `pat_phone` varchar(200) DEFAULT NULL,
   `pat_type` varchar(200) DEFAULT NULL,
+  `pat_date_reg` datetime DEFAULT NULL,
   `pat_date_joined` timestamp(6) NOT NULL DEFAULT current_timestamp(6) ON UPDATE current_timestamp(6),
   `pat_condition` varchar(255) DEFAULT NULL,
   `pat_discharge_status` varchar(200) DEFAULT NULL,
@@ -110,9 +120,8 @@ CREATE TABLE `his_patients` (
 -- Dumping data for table `his_patients`
 --
 
-INSERT INTO `his_patients` (`pat_id`, `pat_fname`, `pat_lname`, `pat_dob`, `pat_age`, `pat_number`, `pat_addr`, `pat_phone`, `pat_type`, `pat_date_joined`, `pat_condition`, `pat_discharge_status`, `pat_dept`, `ref_unit`, `pat_treatment`) VALUES
-(28, 'Lycka Joy ', 'Baylon', '06/24/2004', '19', '9782I', 'bahay', '1', 'Active', '2025-05-27 02:40:16.698306', 'kulang sa coke', NULL, 'department of the ph', 'doctor strange', 'coke'),
-(29, 'Aaron', 'Quimzon', '1/1/2004', '21', 'CEHMV', 'bahay nila', '2', 'Active', '2025-05-27 02:41:15.918947', 'unknown', NULL, 'department of labor', 'doctor stone', 'unknown');
+INSERT INTO `his_patients` (`pat_id`, `pat_fname`, `pat_lname`, `pat_dob`, `pat_age`, `pat_gender`, `pat_number`, `pat_addr`, `pat_phone`, `pat_type`, `pat_date_reg`, `pat_date_joined`, `pat_condition`, `pat_discharge_status`, `pat_dept`, `ref_unit`, `pat_treatment`) VALUES
+(31, 'Large', 'Calagno', '2004-05-03', '21', 'Male', 'OSMDT', '1', '1', 'Inactive', '2025-06-18 18:28:13', '2025-06-18 18:09:35.852490', '1', NULL, '1', '1', '1');
 
 -- --------------------------------------------------------
 
@@ -135,6 +144,18 @@ CREATE TABLE `his_patient_transfers` (
 
 INSERT INTO `his_patient_transfers` (`t_id`, `t_hospital`, `t_date`, `t_pat_name`, `t_pat_number`, `t_status`) VALUES
 (1, 'Kenyatta National Hospital', '2020-01-11', 'Mart Developers', '9KXPM', 'Success');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `his_pwdresets`
+--
+
+CREATE TABLE `his_pwdresets` (
+  `id` int(20) NOT NULL,
+  `email` varchar(200) NOT NULL,
+  `token` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Indexes for dumped tables
@@ -172,6 +193,12 @@ ALTER TABLE `his_patient_transfers`
   ADD PRIMARY KEY (`t_id`);
 
 --
+-- Indexes for table `his_pwdresets`
+--
+ALTER TABLE `his_pwdresets`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -185,25 +212,31 @@ ALTER TABLE `his_admin`
 -- AUTO_INCREMENT for table `his_consultations`
 --
 ALTER TABLE `his_consultations`
-  MODIFY `consult_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `consult_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- AUTO_INCREMENT for table `his_docs`
 --
 ALTER TABLE `his_docs`
-  MODIFY `doc_id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `doc_id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 
 --
 -- AUTO_INCREMENT for table `his_patients`
 --
 ALTER TABLE `his_patients`
-  MODIFY `pat_id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
+  MODIFY `pat_id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
 
 --
 -- AUTO_INCREMENT for table `his_patient_transfers`
 --
 ALTER TABLE `his_patient_transfers`
   MODIFY `t_id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `his_pwdresets`
+--
+ALTER TABLE `his_pwdresets`
+  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT;
 
 --
 -- Constraints for dumped tables
